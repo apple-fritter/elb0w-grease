0ffeacc6140c43f3cb002e857eaaf86049edbfdd
@apple-fritter <125221355+apple-fritter@users.noreply.github.com>
2023-04-11 16:26:22 -0700

    Update chrome_privacy.sh

--- chrome_privacy.sh
+++ chrome_privacy.sh
@@ -4,44 +4,40 @@
 adb shell pm clear com.android.chrome
 
 # Enable Do Not Track
-adb shell "echo 'chrome://settings/privacy' > /sdcard/chrome.txt"
-adb shell "am start -a android.intent.action.VIEW -d file:///sdcard/chrome.txt"
-adb shell "input tap 155 260"
-adb shell "input tap 155 330"
+adb shell settings put global privacy_do_not_track_enabled 1
 
 # Disable WebRTC
-adb shell "echo 'chrome://flags/#disable-webrtc' > /sdcard/chrome.txt"
-adb shell "am start -a android.intent.action.VIEW -d file:///sdcard/chrome.txt"
-adb shell "input tap 350 530"
-adb shell "input tap 350 630"
-adb shell "input tap 350 1080"
-adb shell "input keyevent 4"
+adb shell settings put global webview_enable_webrtc 0
 
 # Block third-party cookies
-adb shell "echo 'chrome://settings/siteData' > /sdcard/chrome.txt"
-adb shell "am start -a android.intent.action.VIEW -d file:///sdcard/chrome.txt"
-adb shell "input tap 155 1075"
-adb shell "input tap 155 1320"
-adb shell "input tap 155 1600"
-adb shell "input keyevent 4"
+adb shell settings put global third_party_cookies_enabled 0
 
 # Disable autofill
-adb shell "echo 'chrome://settings/autofill' > /sdcard/chrome.txt"
-adb shell "am start -a android.intent.action.VIEW -d file:///sdcard/chrome.txt"
-adb shell "input tap 155 710"
-adb shell "input tap 155 890"
+adb shell settings put secure autofill_enabled 0
 
 # Disable location sharing
-adb shell "echo 'chrome://settings/content/location' > /sdcard/chrome.txt"
-adb shell "am start -a android.intent.action.VIEW -d file:///sdcard/chrome.txt"
-adb shell "input tap 155 480"
-adb shell "input tap 155 590"
+adb shell settings put global location_settings_enabled 0
 
 # Disable push notifications
-adb shell "echo 'chrome://settings/content/notifications' > /sdcard/chrome.txt"
-adb shell "am start -a android.intent.action.VIEW -d file:///sdcard/chrome.txt"
-adb shell "input tap 155 480"
-adb shell "input tap 155 590"
+adb shell settings put global pushmessaging_enabled 0
 
-# Close Chrome settings
-adb shell "input keyevent 4"
+# Block camera and microphone access
+adb shell settings put global camera_disabled 1
+adb shell settings put global microphone_disabled 1
+
+# Block usage data sharing
+adb shell settings put global usage_stats_enabled 0
+
+# Allow first-party cookies only
+adb shell settings put global cookie_setting 1
+
+# Set default browsing mode to incognito
+adb shell "echo '
+<map>
+  <entry key=\"browser_mode_incognito\" value=\"true\" />
+</map>
+' > /data/user_de/0/com.android.chrome/shared_prefs/ChromeSyncPrefs.xml"
+
+# Restart Chrome
+adb shell am force-stop com.android.chrome
+adb shell am start -n com.android.chrome/com.google.android.apps.chrome.Main
